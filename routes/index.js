f827a47e7f35511c0f4b7b358a10982ab2a5e1d9
Sergio Festa <sfestatomari@gmail.com>
2019-07-25 22:52:44 -0300

    add nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const nodemailer = require('nodemailer');
+const sparkPostTransport = require('nodemailer-sparkpost-transport');
 
-/* GET home page */
-router.get('/', (req, res, next) => {
+const transporter = nodemailer.createTransport(sparkPostTransport({
+  'sparkPostApiKey': process.env.SPARKPOST_API_KEY
+}));
+
+router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.post('/send-email', (req, res) => {
+  let { username, email } = req.body;
+  transporter.sendMail({
+    from: '"Bravocado! 🥑" <bravocado@ironhackers.dev>',
+    to: email, 
+    subject: 'Awesome Subject', 
+    text: 'Awesome Message',
+    html: `<b>${username}</b>`
+  })
+  .then(info => {
+    res.redirect('/')
+  })
+  .catch(error => console.log(error))
 });
 
 module.exports = router;
